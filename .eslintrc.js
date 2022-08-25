3d2897af4a98f2468f84d0c5b629dbe3ac7b38cc
lixiangui <lixianguiwo@126.com>
2022-08-25 17:39:20 +0800

    husky

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,33 @@
+/** @format */
+
 module.exports = {
   parser: '@typescript-eslint/parser',
   extends: [
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended'
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
   ],
   // 使用推荐的React代码检测规范
   plugins: ['@typescript-eslint'],
   env: {
-      browser: true,
-      node: true,
+    browser: true,
+    node: true,
   },
   // 自动发现React的版本，从而进行规范react代码
   settings: {
-      "react": {
-          "pragma": "React",
-          "version": "detect"
-      }
+    react: {
+      pragma: 'React',
+      version: 'detect',
+    },
   },
   // 指定ESLint可以解析JSX语法
   parserOptions: {
-      "ecmaVersion": 2019,
-      "sourceType": 'module',
-      "ecmaFeatures": {
-          jsx: true
-      }
+    ecmaVersion: 2019,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
-  rules: {
-
-  }
-};
+  rules: {},
+}
