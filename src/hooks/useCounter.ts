aff23cef827a4ff18a0ccc11db2c4fa1a85148f0
lixiangui <lixianguiwo@126.com>
2022-12-29 18:00:52 +0800

    fix: es6

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,6 +1,6 @@
 import { useCallback, useState } from 'react'
 
-function useCounter() {
+const useCounter = () => {
   const [count, setCount] = useState(0)
   const increment = useCallback(() => setCount(count + 1), [count])
   const decrement = useCallback(() => setCount(count - 1), [count])
